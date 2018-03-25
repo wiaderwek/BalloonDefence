8462ae3055331d5461f8e7d055f44fef869c9d0a
Tomasz Wiaderek <t.wiaderwek@gmail.com>
2018-03-25 19:02:31 +0200

    Initial commit

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+        GridPane grid = new GridPane();
+        Scene scene = new Scene(grid, 600, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
